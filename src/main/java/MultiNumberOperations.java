5f7cad9de4eaa67eb786647778cf1bac57fa5910
Pierson Silver <silverpierson1@gmail.com>
2024-09-03 13:13:21 -0400

    Add MultiNumberOperations and MultiNumberOperationsTest classes

---
+++ src/main/java/MultiNumberOperations.java
@@ -0,0 +1,15 @@
+public class MultiNumberOperations {
+    public String calculate(String quat1, String operand, String quat2){
+        Conversions converter = new Conversions();
+        int num1 = converter.quatToDec(quat1);
+        int num2 = converter.quatToDec(quat2);
+        int result = switch(operand){
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> 0;
+        };
+        return converter.decToQuat(result);
+    }
+}
---
+++ src/test/java/MultiNumberOperationsTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MultiNumberOperationsTest {
+    MultiNumberOperations test = new MultiNumberOperations();
+    @Test
+    public void additionTest(){
+        String quat1 = "210"; //36
+        String quat2 = "22"; //10
+        Assertions.assertEquals("232", test.calculate(quat1, "+", quat2));
+    }
+    @Test
+    public void subtractionTest(){
+        String quat1 = "210"; //36
+        String quat2 = "22"; //10
+        Assertions.assertEquals("122", test.calculate(quat1, "-", quat2));
+    }
+    @Test
+    public void multiplicationTest(){
+        String quat1 = "210"; //36
+        String quat2 = "22"; //10
+        Assertions.assertEquals("11220", test.calculate(quat1, "*", quat2));
+    }@Test
+    public void divisionTest(){
+        String quat1 = "210"; //36
+        String quat2 = "22"; //10
+        Assertions.assertEquals("3", test.calculate(quat1, "/", quat2));
+    }
+
+}
