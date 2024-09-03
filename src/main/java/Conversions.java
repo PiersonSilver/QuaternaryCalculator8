cb416a12f4c93aebed5ab52dcd456536a14737fd
Pierson Silver <silverpierson1@gmail.com>
2024-09-03 12:39:06 -0400

    Add Conversions Class and ConversionsTest Class Remove empty 'idk' classes

---
+++ src/main/java/Conversions.java
@@ -0,0 +1,22 @@
+public class Conversions {
+    public String decToQuat(int dec){
+        StringBuilder converted = new StringBuilder();
+        while(dec > 0){
+            int remainder = dec % 4;
+            dec = (dec - remainder)/4;
+            converted.append(remainder);
+        }
+        converted.reverse();
+        return converted.toString();
+    }
+
+    public int quatToDec(String quat){
+        String[] quatArray = quat.split("");
+        int converted = 0;
+        for(int i = 0; i <= quatArray.length-1; i++){
+            converted += Integer.parseInt(quatArray[quatArray.length-1 - i]) * (int)Math.pow(4,i);
+        }
+        return converted;
+    }
+
+}
---
+++ src/test/java/ConversionsTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConversionsTest {
+    Conversions test = new Conversions();
+    @Test
+    public void decToQuatTest(){
+        int input = 153;
+        Assertions.assertEquals("2121", test.decToQuat(input));
+    }
+    @Test
+    public void quatToDecTest(){
+        String input = "2121";
+        Assertions.assertEquals(153, test.quatToDec(input));
+    }
+    @Test
+    public void backAndForthTest(){
+        int input = 579;
+        String quatFromInput = test.decToQuat(input);
+        Assertions.assertEquals(input, test.quatToDec(quatFromInput));
+    }
+}
