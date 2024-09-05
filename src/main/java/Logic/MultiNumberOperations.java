169addfe54638a6e7c65d263d940d049cad99017
caleb.reyes <caleb.reyes@bsu.edu>
2024-09-05 12:54:02 -0400

    Add squared and squareroot functions to calculator switch case.

--- src/main/java/Logic/MultiNumberOperations.java
+++ src/main/java/Logic/MultiNumberOperations.java
@@ -3,6 +3,7 @@ package Logic;
 public class MultiNumberOperations {
     public String calculate(String quat1, String operand, String quat2){
         Conversions converter = new Conversions();
+        SingleButtonFunctions functions = new SingleButtonFunctions();
         int num1 = converter.quatToDec(quat1);
         int num2 = converter.quatToDec(quat2);
         int result = switch(operand){
@@ -10,6 +11,8 @@ public class MultiNumberOperations {
             case "-" -> num1 - num2;
             case "*" -> num1 * num2;
             case "/" -> num1 / num2;
+            case "^" -> functions.squared(num1);
+            case "%" -> functions.squareroot(num1);
             default -> 0;
         };
         return converter.decToQuat(result);
